699fc69c00cdcc6a660578214056256d92b52a69
shamkhal <70231829+shamkhall@users.noreply.github.com>
2026-04-01 16:28:34 +0400

    feat: configure main.ts as gRPC microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'carpoint',
+        protoPath: join(__dirname, '../proto/car-point.proto'),
+        url: process.env.GRPC_URL ?? '0.0.0.0:5000',
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
